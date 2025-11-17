c02592d93448dea4e49f20c43caae3e1254ba254
tempest2234 <144287030+tempest2234@users.noreply.github.com>
2025-11-17 04:38:56 -0500

    push

--- firebase.js
+++ firebase.js
@@ -1,17 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY_HERE",
-  authDomain: "YOUR_DOMAIN_HERE",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCugBIWal63vTuVIWnJHrTn5AkUl6AT99w",
+  authDomain: "crazytempest-5c45a.firebaseapp.com",
+  projectId: "crazytempest-5c45a",
+  storageBucket: "crazytempest-5c45a.firebasestorage.app",
+  messagingSenderId: "569571577012",
+  appId: "1:569571577012:web:8f7d942cc09af9681f9be1"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
